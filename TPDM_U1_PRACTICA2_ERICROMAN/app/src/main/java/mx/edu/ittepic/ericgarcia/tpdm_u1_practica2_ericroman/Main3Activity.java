eafe1c9d28fd024fb20ab200c44cef7cb012a891
Unknown <ericromangarcial@gmail.com>
2019-02-12 21:27:00 -0600

    PPP

---
+++ TPDM_U1_PRACTICA2_ERICROMAN/app/src/main/java/mx/edu/ittepic/ericgarcia/tpdm_u1_practica2_ericroman/Main3Activity.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.ericgarcia.tpdm_u1_practica2_ericroman;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
